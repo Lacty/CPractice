e478d91dfcce6b38c875016bc9ad174996303060
akira <akira@DESKTOP-PQLH5HN>
2024-04-02 19:53:46 +0900

    プロジェクト ファイルを追加します。

---
+++ CPractice/CPractice.cpp
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main() {
+	int age;
+	printf("年齢を入力してください");
+	scanf("%d", &age);
+
+	printf("あなたの生まれてから現在までのおおよその日数は%d日です", age * 365);
+
+	return 0;
+}
